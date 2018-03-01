9cf9398e76c21d6ad93eb73affd70f0e662edf85
Peter Kordel <pkordel@gmail.com>
2018-03-01 15:28:57 +0700

    Now use actioncable to stream with sockets

--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,2 +1,24 @@
+import { setCallback } from "client/chat";
 import "components/message/message";
 import "./messages.css";
+
+function scrollToBottom(element) {
+  // eslint-disable-next-line
+  element.scrollTop = element.scrollHeight;
+}
+
+const messages = document.querySelector(".js-messages");
+
+if (messages) {
+  const content = messages.querySelector(".js-messages--content");
+
+  scrollToBottom(content);
+
+  // Telling chat.js to call this block of code whenever a new message is
+  // received
+  setCallback(message => {
+    content.insertAdjacentHTML("beforeend", message);
+
+    scrollToBottom(content);
+  });
+}
